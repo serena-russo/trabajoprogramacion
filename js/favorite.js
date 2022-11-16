6f0f9463b27209b17585a1a61e0334e7e66ba805
srusso@udesa.edu.ar <srusso@udesa.edu.ar>
2022-11-16 20:23:55 -0300

    nomesale el 1

---
+++ js/favorite.js
@@ -0,0 +1,17 @@
+/// formulario principal 
+
+///if (serena.lenght == 0);
+///    let alert1 = document.querySelector(".serena");
+  ///  probando.addEventListener("submit" , function(){
+   ///  alert("el campo de busqueda esta vacio");
+  ///  })
+///elif (serena.lenght<3);
+   /// let alert2 = document.querySelector(".serena");
+   /// probando.addEventListener("submit" , function(){
+  ///      alert("el termino buscado debe tener al menos tres caracteres");
+   // })
+
+let alert1 = document.querySelector(".serena");
+probando.addEventListener("click" , function(){
+    alert("el campo de busqueda esta vacio");
+})

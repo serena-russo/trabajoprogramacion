c491c4c3fc53e53dc340427df6d2edb3f048817a
luciana <lzeitune@udesa.edu.ar>
2022-11-20 13:39:13 -0300

    avance favoritos

--- js/favorite.js
+++ js/favorite.js
@@ -1,16 +1,62 @@
 let storage = localStorage.getItem("favoritos") 
 let favorite = JSON.parse(storage); 
 
+let listaPeliculas = document.querySelector(".pelisul");
+let listaSeries = document.querySelector(".seriesul"); 
+
 let section = document.querySelector(".containerfavortos")
 
 let peliculasFavs = []
 
-if (favorite.length == 0){
+if ( favorite == null || favorite.length == 0) {
     section.innerHTML = '<p>No hay peliculas en favoritos</p>' 
 }
 else{
     for (let i = 0; i < favorite.length; i++) {
-        let url = peliculasFavs[i]; 
-    }
-} 
+        let urlPelis = `https://api.themoviedb.org/3/movie/${favoritos[i]}?api_key=${api_key}&language=en-US` 
 
+        
+        fetch(urlPelis)
+            .then(function (respuesta) {
+                return respuesta.json()
+
+            })
+            .then(function (data) {
+                console.log(data)
+
+                peliculasFavs += //FALTA ALGO
+                listaPeliculas.innerHTML = peliculasFavs;
+})
+            .catch(function (error) {
+                return error
+
+        })
+    }
+}
+
+let seriesFavs = []
+if (favoritos == null || favoritos.length == 0) {
+    section.innerHTML = '<p> No hay series en favoritos </p>'
+
+} else {
+    for (let i = 0; i < favoritos.length; i++) {
+        let urlSeries = `https://api.themoviedb.org/3/tv/${favoritos[i]}?api_key=${api_key}&language=en-US`
+
+        fetch(urlSeries)
+            .then(function (respuesta) {
+                return respuesta.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                seriesFavs += // FALTA ALGO
+                listaSeries.innerHTML = seriesFavs;
+
+
+            })
+            .catch(function (error) {
+                return error
+
+            })
+
+    }
+}

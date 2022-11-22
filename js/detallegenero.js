162160a7bdc434016cb3b62b6df72d3a6bb7fbea
srusso@udesa.edu.ar <srusso@udesa.edu.ar>
2022-11-22 18:38:52 -0300

    sere::no me saleeeee

--- js/detallegenero.js
+++ js/detallegenero.js
@@ -1,22 +1,22 @@
 //Endpoints
-let urldiscovermovie = `https://api.themoviedb.org/3/discover/movie?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate` 
+let urldiscovermovie = `https://api.themoviedb.org/3/discover/movie?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
 let urldiscovertv = `https://api.themoviedb.org/3/discover/tv?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
-
-//Capturar la qs y ApiKey
+ 
+//Capturar la qs
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
-let idGenero = qsObj.get('idGenero');
-let api_key = "cd221d9b379938868090204c71bbef7e"
-
-//Capturar elementos 
+let idGenero = qsObj.get('id');
+ 
+ 
+//Capturar elementos
 let lista_generos_peliculas = document.querySelector(".lgp")
 let lista_generos_serie = document.querySelector(".lgs")
 let h2Peliculas = document.querySelector(".otroclass1")
 let h2Series = document.querySelector(".otroclass2")
-
-
+ 
+ 
 //Fetch de generos peliculas
-fetch (urldiscovermovie) 
+fetch (urldiscovermovie)
 .then(function(res){
     return res.json()
 })
@@ -26,12 +26,7 @@ fetch (urldiscovermovie)
     if (peliculas.length > 0){
         let contenido = ""
         for (let i = 0; i<5; i++){
-            contenido += ` falta aca 
-            
-            
-            
-            
-            
+            contenido += `
             `
         };
         lista_generos_peliculas.innerHTML = contenido;
@@ -42,9 +37,9 @@ fetch (urldiscovermovie)
 .catch(function(error){
     return error
 })
-
-
-//Fetch de generos series 
+ 
+ 
+//Fetch de generos series
 fetch(urldiscovertv)
 .then(function(res){
     return res.json()
@@ -55,10 +50,10 @@ fetch(urldiscovertv)
     if (series.length > 0) {
         let contenido2 = ""
         for (let i = 0; i <5; i++) {
-            contenido2 += `falta aca 
-            
-            
-            
+            contenido2 += `falta aca
+           
+           
+           
             `
         };
         lista_generos_serie.innerHTML = contenido2;
@@ -69,4 +64,4 @@ fetch(urldiscovertv)
 .catch(function(error){
     return error
 })
-
+ 
--- js/search-results.js
+++ js/search-results.js
@@ -15,7 +15,7 @@ fetch (urlsearchmovie)
     let lista_peliculas = []
 
     for (let i = 0 ; i<5; i++){
-        lista_peliculas +=` <article><a class="popularesinicio" href="./resenapelis.html?id=${index_peliculas[i].id}"><img class="imagenapisearch" src="https://image.tmdb.org/t/p/w500${index_peliculas[i].poster_path}" alt="${index_peliculas[i].poster_path}"><p class="popularesinicio nombrepeliapi">${index_peliculas[i].title}</p></a></article>
+        lista_peliculas +=` <article><a class="popularesinicio" href="./resenapelis.html?id=${index_peliculas[i].id}"><img class="imagenes" src="https://image.tmdb.org/t/p/w500${index_peliculas[i].poster_path}" alt="${index_peliculas[i].poster_path}"><p class="popularesinicio nombrepeliapi">${index_peliculas[i].title}</p></a></article>
         <p class="releaseDate popularesinicio">${index_peliculas[i].release_date}</p>`
     }
     contenedor.innerHTML = lista_peliculas
@@ -38,7 +38,7 @@ fetch (urlsearchtv)
     let lista_peliculas = []
 
     for (let i = 0 ; i<5; i++){
-        lista_peliculas +=` <article><a class="popularesinicio" href="./resenaseries.html?id=${index_peliculas[i].id}"><img class="imagenapisearch" src="https://image.tmdb.org/t/p/w500${index_peliculas[i].poster_path}" alt="${index_peliculas[i].poster_path}"><p class="popularesinicio nombrepeliapi">${index_peliculas[i].title}</p></a></article>
+        lista_peliculas +=` <article><a class="popularesinicio" href="./resenaseries.html?id=${index_peliculas[i].id}"><img class="imagenes" src="https://image.tmdb.org/t/p/w500${index_peliculas[i].poster_path}" alt="${index_peliculas[i].poster_path}"><p class="popularesinicio nombrepeliapi">${index_peliculas[i].title}</p></a></article>
         <p class="releaseDate popularesinicio">${index_peliculas[i].first_air_date}</p>`
     }
     contenedor.innerHTML = lista_peliculas

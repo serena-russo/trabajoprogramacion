2a62fe28fdcea169a09bb175d97169dc64f2efa7
luciana <lzeitune@udesa.edu.ar>
2022-11-22 22:55:08 -0300

    algo de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,15 +1,20 @@
 let formulario= document.querySelector("form");
+
 let campoForm= document.querySelector("[name=buscador]");
+campoForm.addEventListener('blur', function(){ // cuando toque fuera del campo, este se vacie 
+    campoForm.value = ""
+})
+
 
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    if ( campoForm.value == "") {
-        alert("Debes escribir algo")
+    if (campoForm.value == "") { // chequeo si el campo esta completo
+        alert("Debes escribir algo") 
     } else if (campoForm.value.length <3){
         alert("Su búsqueda debe tener más de tres caracteres")
     }else {
-        this.submit()
+        this.submit() // funcion de javascript que se encarga de mandar el formulario si no hay errores
     }
 
     
